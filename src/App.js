ef6c2b1a61d34af4ea168569eeba9d56c0dd360b
Adam <adamomarali37@gmail.com>
2022-06-23 03:16:21 -0400

    Favicon update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className=''>A blank canvas. Create what you want. More coming soon...</p>
       </header>
     </div>
   );
